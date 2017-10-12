7244c5e70f28a15420a9c7224f139561a4710222
Garett Haight <Garett.Haight@altsrc.net>
2017-10-12 10:41:42 -0700

    Added comic book detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -9,12 +9,7 @@ namespace ComicBookGallery.Controllers
     public class ComicBooksController : Controller
     {
         public ActionResult Detail() {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from the comic books controller");
+            return View();
         }
     }
 }
